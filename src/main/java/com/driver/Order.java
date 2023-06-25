e8435e312eb6f1aafeeeb804461f28e8c005418a
Ishita Arora <64803334+ishita-arora28@users.noreply.github.com>
2023-06-25 10:04:32 +0000

    New Message

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,6 +9,12 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        String[] currDelTime=deliveryTime.split(":");
+        int hr=Integer.parseInt(currDelTime[0]);
+        int sec=Integer.parseInt(currDelTime[1]);
+        System.out.println(hr*60+sec);
+        
+
     }
 
     public String getId() {
